c78cdd4aa3302a48c9e659fb853a6f625f89c51c
ccsd-coder <julien.esposito@cc-sd.edu>
2016-02-24 11:32:51 -0800

    Question 13
    Using double ordering, look up page 114 for reference.

--- premiere_exercise.sql
+++ premiere_exercise.sql
@@ -1,4 +1,6 @@
--- Question 12
+-- Question 13
 -- -----------
-select * from part order by description asc;
+-- This means that the main ordering is performed on the 
+-- wharehouse attribute, and THEN at the part_num level.
+select * from part order by warehouse asc, part_num asc;
 
