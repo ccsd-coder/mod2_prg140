5003aa321c8eb8809849002258ddd0484b0e213e
ccsd-coder <julien.esposito@cc-sd.edu>
2016-02-23 21:46:52 -0800

    Q9
    Q9

--- premiere_exercise.sql
+++ premiere_exercise.sql
@@ -1,5 +1,7 @@
--- Question 08
-select order_num, order_date,
- part_num, description, class
- from orders, part 
- order by class asc, order_num asc;
+
+ 
+ -- Question 09
+ select rep_num, last_name, first_name 
+ from rep
+ where rep_num in 
+ ( select rep_num from customer where credit_limit = 10000 );
