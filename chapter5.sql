e445ac8c90306b3f5ab87173cdf4e9a6ffb9778e
ccsd-coder <julien.esposito@cc-sd.edu>
2016-02-24 12:16:22 -0800

    Question 1
    Question 1

--- chapter5.sql
+++ chapter5.sql
@@ -1,2 +1,9 @@
 -- Solving Chapter 5 - Premiere Problem
 -- ------------------------------------
+
+-- Question 01
+-- -----------
+select o.order_num, o.order_date,
+	c.customer_num, c.customer_name
+	from customer c, orders o
+	where ( c.customer_num = o.customer_num );
