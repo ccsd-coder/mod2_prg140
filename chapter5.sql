f116213b83910657be4fb0f5ef1c828ae8f04a47
ccsd-coder <julien.esposito@cc-sd.edu>
2016-02-24 21:48:05 -0800

    Question 03
    Question 03

--- chapter5.sql
+++ chapter5.sql
@@ -1,5 +1,8 @@
 -- Solving Chapter 5 - Premiere Problem
 -- ------------------------------------
 
--- Question 02
+-- Question 03
 -- -----------
+select o.order_num, o.order_date, ol.part_num,
+ ol.num_ordered, ol.quoted_price from orders o,
+ order_line ol where ( ol.order_num = o.order_num );
