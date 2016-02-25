167a9849470a4504f8e5556145f387c81c6493a9
ccsd-coder <julien.esposito@cc-sd.edu>
2016-02-25 11:00:46 -0800

    Question 5
    Using the IN operator

--- chapter5.sql
+++ chapter5.sql
@@ -1,13 +1,11 @@
 -- Solving Chapter 5 - Premiere Problem
 -- ------------------------------------
 
--- Question 04 ( Using a JOIN Query)
+-- Question 05 ( Using IN)
 -- -----------
 -- customer
 -- orders
 
-select o.order_num, c.customer_name, o.order_date
-from customer c, orders o
-
-where ( c.customer_num = o.customer_num )
- and (o.order_date = '2010-10-23');
+ 
+ select order_num from orders where customer_num IN
+ ( select customer_num from orders where ( order_date = '2010-10-23' ) );
